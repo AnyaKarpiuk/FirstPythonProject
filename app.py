8414d5461a8b4e9ac782fe0be177988b57d8e596
AnyaKarpiuk <anna-19.1994@mail.ru>
2021-10-13 14:32:20 +0300

    compare name length

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-name = input('What is your name? ')
-color = input('What is your favorite color? ')
-print(name + ' likes ' + color)
+# request a name
+name = input('Print your name: ')
+
+# compare name's length
+if len(name) <= 3:
+    print('We are sorry!')
+    print('Name must be at least 4 characters')
+    print(f'Name {name} has {len(name)} characters')
+elif len(name) > 15:
+    print('We are sorry!')
+    print('Name must be maximum 15 characters')
+    print(f'Name {name} has {len(name)} characters')
+else:
+    print(f'Thank you, {name}')
+
+
+
